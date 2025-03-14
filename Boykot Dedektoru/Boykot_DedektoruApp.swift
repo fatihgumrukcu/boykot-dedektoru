7bcfa18d10646a13bc750a97d5469954eb2f02e0
fatihgumrukcci <fatih.gmrkc@gmail.com>
2025-03-14 18:37:26 +0300

    Initial Commit

---
+++ Boykot Dedektoru/Boykot_DedektoruApp.swift
@@ -0,0 +1,17 @@
+//
+//  Boykot_DedektoruApp.swift
+//  Boykot Dedektoru
+//
+//  Created by Fatih Gümrükçü on 14.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct Boykot_DedektoruApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
